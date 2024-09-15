f02352fc581ba9610be3f34c58dd6cb0d76f8b7f
Gordian <gordianbahri@gmail.com>
2024-09-15 11:19:49 +0200

    Initial commit

---
+++ themes/ayudark.h
@@ -0,0 +1,8 @@
+// Ayu Dark
+// clang-format off
+
+static const char col_gray1[] = "#0B0E14";
+static const char col_gray2[] = "#0B0E14";
+static const char col_gray3[] = "#BFBDB6";
+static const char col_gray4[] = "#0F131A";
+static const char col_accnt[] = "#E6B450";
---
+++ themes/ayumirage.h
@@ -0,0 +1,8 @@
+// mh's them
+// clang-format off
+
+static const char col_gray1[] = "#171B24";
+static const char col_gray2[] = "#171B24";
+static const char col_gray3[] = "#CCCAC2";
+static const char col_gray4[] = "#1C212B";
+static const char col_accnt[] = "#FFCC66";
